2035da1f8f97b26a0e2f96fa2679633b568e0c78
JeongHanJun <54197360+JeongHanJun@users.noreply.github.com>
2021-02-26 21:37:14 +0900

    정수 삼각형

---
+++ Python 3 PS Code/BOJ/1932.py
@@ -0,0 +1,34 @@
+# 1932 정수 삼각형
+from sys import stdin
+n = int(stdin.readline())
+dp = []
+for i in range(n):
+    dp.append(list(map(int, stdin.readline().rstrip().split())))
+
+for i in range(1, n):
+    for j in range(len(dp[i])):
+        if j == 0:
+            dp[i][j] += dp[i-1][j]
+        elif j == len(dp[i])-1:
+            dp[i][j] += dp[i-1][j-1]
+        else:
+            dp[i][j] += max(dp[i-1][j-1], dp[i-1][j])
+print(max(dp[n-1]))
+
+""" 
+from sys import stdin
+n = int(stdin.readline())
+nums = []
+dp = []
+for i in range(n):
+    dp.append(list(range(i+1)))
+    nums.append(list(map(int, stdin.readline().rstrip().split())))
+print(dp)
+print(nums)
+dp[0] = nums[0]
+temp = dp[0]
+for i in range(1,n):
+    for j in range(1, i+2):
+        dp[i][j] += max(nums[i-1][j-1] + nums[i-1][j])
+    print(dp)
+"""

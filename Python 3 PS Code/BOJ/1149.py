9a9fc67e33bd141d73dcc4152530b92ce05dd8c2
JeongHanJun <54197360+JeongHanJun@users.noreply.github.com>
2021-02-26 21:38:49 +0900

    RGB거리

---
+++ Python 3 PS Code/BOJ/1149.py
@@ -0,0 +1,15 @@
+# 1149 RGB거리
+from sys import stdin
+n = int(stdin.readline())
+costs = []
+dp = [[0] * 3 for _ in range(n)]
+for _ in range(n):
+    costs.append(list(map(int, stdin.readline().rstrip().split())))
+dp[0][0] = costs[0][0]
+dp[0][1] = costs[0][1]
+dp[0][2] = costs[0][2]
+for i in range(1,n):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i][1]
+    dp[i][2] = min(dp[i-1][1], dp[i-1][0]) + costs[i][2]
+print( min(dp[n-1][0], min(dp[n-1][1], dp[n-1][2])) )

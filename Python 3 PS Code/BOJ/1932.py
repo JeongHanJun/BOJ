cd5f65929cba05d90e8bef13f5b3d784c4550b0b
JeongHanJun <54197360+JeongHanJun@users.noreply.github.com>
2021-03-03 14:13:56 +0900

    정수 삼각형

--- Python 3 PS Code/BOJ/1932.py
+++ Python 3 PS Code/BOJ/1932.py
@@ -32,3 +32,22 @@ for i in range(1,n):
         dp[i][j] += max(nums[i-1][j-1] + nums[i-1][j])
     print(dp)
 """
+"""
+# 새로운 리스트에 dp를 진행
+from sys import stdin
+n = int(stdin.readline())
+nums = []
+for _ in range(n):
+    nums.append(list(map(int, stdin.readline().rstrip().split())))
+dp = [ list(range(i+1)) for i in range(n) ]
+dp[0] = nums[0]
+for i in range(1,n):
+    for j in range(len(dp[i])):
+        if j == 0:
+            dp[i][j] = dp[i-1][j] + nums[i][j]
+        elif j == len(dp[i]) - 1:
+            dp[i][j] = dp[i-1][j-1] + nums[i][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + nums[i][j]
+print(max(dp[n-1]))
+"""
